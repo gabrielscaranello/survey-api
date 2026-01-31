22fde2c5f83966beb2cc59e1c18246d7e83a4859
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2026-01-31 17:48:31 -0300

    refactor: create custom errors

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,13 +1,14 @@
+import { MissingParamError } from '../errors'
 import type { HttpRequest, HttpResponse } from '../protocols'
 
 export class SignupController {
   handle(httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body?.name) {
-      return { statusCode: 400, body: new Error('Missing param: name') }
+      return { statusCode: 400, body: new MissingParamError('name') }
     }
 
     if (!httpRequest.body.email) {
-      return { statusCode: 400, body: new Error('Missing param: email') }
+      return { statusCode: 400, body: new MissingParamError('email') }
     }
   }
 }
