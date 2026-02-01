553dce6d7a7d0aaf67aad0a64f1d88a60f4a3233
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2026-01-31 21:50:50 -0300

    test: ensure SignUpController returns 500 if AddAccount throws

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -212,4 +212,24 @@ describe('SignUp Controller', () => {
       password: httpRequest.body.password
     })
   })
+
+  it('should return 500 if addAccount throws', () => {
+    const { sut, addAccountStub } = makeSut()
+    vi.spyOn(addAccountStub, 'add').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(HTTPStatusCode.SERVER_ERROR)
+    expect(httpResponse.body).toEqual(new ServerError())
+  })
 })
