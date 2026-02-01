0f26b849c9f624921fd88e6b08d7a03349e5ddfc
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2026-02-01 02:19:33 -0300

    test: ensure DbAddAccount calls hasher with correct values

---
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -0,0 +1,40 @@
+import type { Hasher } from '@/data/protocols'
+
+import { DbAddAccount } from './db-add-account'
+
+interface SutTypes {
+  hasherStub: Hasher
+  sut: DbAddAccount
+}
+
+const makeHasher = (): Hasher => {
+  class HasherStub implements Hasher {
+    async hash(_value: string): Promise<string> {
+      return await Promise.resolve('hashed_value')
+    }
+  }
+
+  return new HasherStub()
+}
+
+const makeSut = (): SutTypes => {
+  const hasherStub = makeHasher()
+  const sut = new DbAddAccount(hasherStub)
+
+  return { sut, hasherStub }
+}
+
+describe('DbAddAccount UseCase', () => {
+  it('should call Hasher with correct password', async () => {
+    const { sut, hasherStub } = makeSut()
+    const hasherSpy = vi.spyOn(hasherStub, 'hash')
+
+    await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+
+    expect(hasherSpy).toBeCalledWith('any_password')
+  })
+})
