a476d11b26084761bd2bdc1f7985abee637f9ff5
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2026-02-01 14:15:59 -0300

    refactor: clean database for earch tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -16,6 +16,11 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.disconnect()
   })
 
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   it('should return an account on success', async () => {
     const sut = makeSut()
 
