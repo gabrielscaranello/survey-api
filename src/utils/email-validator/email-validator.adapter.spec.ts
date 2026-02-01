ce3afe342639aa93e61fca982b8cc93616437e47
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2026-02-01 01:58:04 -0300

    test: ensure EmailValidatorAdapter returns true if validator returns true

--- src/utils/email-validator/email-validator.adapter.spec.ts
+++ src/utils/email-validator/email-validator.adapter.spec.ts
@@ -2,16 +2,32 @@ import type { EmailValidator } from '@/presentation/protocols/email-validator'
 
 import { EmailValidatorAdapter } from './email-validator.adapter'
 
+const mockedIsEmail = vi.fn().mockReturnValue(true)
+
 const makeSut = (): EmailValidator => {
   const emailValidatorAdapter = new EmailValidatorAdapter()
   return emailValidatorAdapter
 }
 
+vi.mock('validator/lib/isEmail', () => ({
+  default: (...args: []): any => mockedIsEmail(...args)
+}))
+
 describe('Email Validator Adapter', () => {
   it('should return false if validator return false', () => {
+    mockedIsEmail.mockReturnValueOnce(false)
     const sut = makeSut()
-    const isValid = sut.isValid('invalid_email.com')
+
+    const isValid = sut.isValid('invalid@email.com')
 
     expect(isValid).toBe(false)
+  })
+
+  it('should return true if validator return true', () => {
+    const sut = makeSut()
+
+    const isValid = sut.isValid('valid@email.com')
+
+    expect(isValid).toBe(true)
   })
 })
