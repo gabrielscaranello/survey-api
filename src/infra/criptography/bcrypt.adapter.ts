2a8975d9f74a383b42d354aa0bf36071b5906edd
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2026-02-01 12:05:04 -0300

    feat: ensure BcryptAdapter returns hashed value on success

--- src/infra/criptography/bcrypt.adapter.ts
+++ src/infra/criptography/bcrypt.adapter.ts
@@ -6,7 +6,7 @@ export class BcryptAdapter implements Hasher {
   constructor(private readonly salt: number) {}
 
   async hash(value: string): Promise<string> {
-    await hash(value, this.salt)
-    return await Promise.resolve(value)
+    const hashedValue = await hash(value, this.salt)
+    return hashedValue
   }
 }
