bd98a24da19f3631ffd760d358a32ec71fed4884
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2026-02-02 00:52:31 -0300

    test: ensure requsts returns content type json as default

---
+++ src/main/middlewares/content-type/content-type.test.ts
@@ -0,0 +1,22 @@
+import request from 'supertest'
+
+import { app } from '@/main/config'
+
+describe('Content-Type Middleware', () => {
+  it('should return default Content-Type as JSON', async () => {
+    const uri = '/test_content_type'
+    app.get(uri, (req, res) => res.send(req.body))
+
+    await request(app).get(uri).expect('Content-Type', /json/)
+  })
+
+  it('should return Content-Type forced', async () => {
+    const uri = '/test_content_type_xml'
+    app.get(uri, (req, res) => {
+      res.type('xml')
+      return res.send(req.body)
+    })
+
+    await request(app).get(uri).expect('Content-Type', /xml/)
+  })
+})
