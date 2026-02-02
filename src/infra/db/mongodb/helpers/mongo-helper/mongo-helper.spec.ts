86d5a0c6da28f0d571e81ceb043da505a9b81cdb
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2026-02-01 21:36:03 -0300

    test: ensure MongoHelper methods works fine

---
+++ src/infra/db/mongodb/helpers/mongo-helper/mongo-helper.spec.ts
@@ -0,0 +1,100 @@
+import { MongoHelper } from './mongo-helper'
+
+const mockedConnect = vi.fn()
+const mockedClose = vi.fn()
+const mockedCollection = vi.fn().mockReturnValue({})
+
+vi.mock('mongodb', () => ({
+  MongoClient: {
+    connect: (...args: []): any => mockedConnect(...args)
+  }
+}))
+
+describe('Mongo Helper', () => {
+  const mockedClient = {
+    close: (...args: []): any => mockedClose(...args),
+    db: () => ({
+      collection: (...args: []): any => mockedCollection(...args)
+    })
+  }
+
+  beforeAll(() => {
+    mockedConnect.mockImplementation(() => mockedClient)
+  })
+
+  afterEach(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  describe('connect', () => {
+    it('should call connect with correct uri', async () => {
+      await MongoHelper.connect('any_url')
+      expect(mockedConnect).toHaveBeenCalledWith('any_url')
+    })
+
+    it('should define client on success', async () => {
+      await MongoHelper.connect('any_url')
+
+      expect(MongoHelper.client).toEqual(mockedClient)
+    })
+
+    it('should throw if MongoClient throws', async () => {
+      mockedConnect.mockRejectedValueOnce(new Error('test error'))
+
+      const promise = MongoHelper.connect('any_url')
+
+      await expect(promise).rejects.toThrow()
+    })
+  })
+
+  describe('disconnect', () => {
+    it('should call client.close', async () => {
+      await MongoHelper.connect('any_url')
+      await MongoHelper.disconnect()
+
+      expect(mockedClose).toHaveBeenCalled()
+      expect(MongoHelper.client).toBeNull()
+    })
+
+    it('should no call client.close if client is not defined', async () => {
+      await MongoHelper.disconnect()
+
+      expect(mockedClose).not.toHaveBeenCalled()
+    })
+
+    it('should throw if client.close throws', async () => {
+      await MongoHelper.connect('any_url')
+      mockedClose.mockRejectedValueOnce(new Error('test error'))
+
+      const promise = MongoHelper.disconnect()
+
+      await expect(promise).rejects.toThrow()
+    })
+  })
+
+  describe('getCollection', () => {
+    it('should throw if client is not defined', () => {
+      const callback = (): any => MongoHelper.getCollection('any_collection')
+
+      expect(callback).toThrow()
+    })
+
+    it('should call client.db with correct values', async () => {
+      await MongoHelper.connect('any_url')
+      MongoHelper.getCollection('any_collection')
+
+      expect(mockedCollection).toHaveBeenCalledWith('any_collection')
+    })
+
+    it('should throw if client.db() throws', async () => {
+      await MongoHelper.connect('any_url')
+      mockedCollection.mockImplementationOnce(() => {
+        throw new Error()
+      })
+
+      const callback = (): any => MongoHelper.getCollection('any_collection')
+
+      expect(callback).toThrow()
+    })
+  })
+})
