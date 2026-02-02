f113a1d6fa7f7971da424dbd28faa500f6fc0a3a
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2026-02-02 00:52:22 -0300

    feat: ensure requsts returns content type json as default

---
+++ src/main/middlewares/content-type/content-type.ts
@@ -0,0 +1,11 @@
+import type { NextFunction, Request, Response } from 'express'
+
+export const contentType = (
+  _: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  res.type('json')
+
+  next()
+}
