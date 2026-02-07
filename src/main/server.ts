4c979be0b6086549c8178f4774a7c324617f79ca
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2026-02-07 11:07:16 -0300

    feat: connect to mongodb before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -4,7 +4,10 @@ import { setupModuleAlias } from './config/module-alias'
 const main = async (): Promise<void> => {
   setupModuleAlias()
 
+  const { MongoHelper } = await import('@/infra/db/mongodb/helpers')
   const { app } = await import('./config/app')
+
+  await MongoHelper.connect(env.db.mongourl)
 
   app.listen(env.app.port, () => {
     process.stdout.write(`Server running at http://localhost:${env.app.port}\n`)
