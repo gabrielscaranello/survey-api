80aa14d135cd14e2edf8d5d87123afdc4a6e48fe
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2026-02-13 00:37:32 -0300

    feat: ensure LogControllerDecorator calls repository when Controller.handle returns server error

--- src/data/protocols/index.ts
+++ src/data/protocols/index.ts
@@ -1,2 +1,3 @@
 export type * from './add-account-repository'
 export type * from './hasher'
+export type * from './log-error-repository'
--- src/main/decorators/log-controller/log-controller.decorator.ts
+++ src/main/decorators/log-controller/log-controller.decorator.ts
@@ -1,14 +1,32 @@
-import type {
-  Controller,
-  HttpRequest,
-  HttpResponse
+import type { LogErrorRepository } from '@/data/protocols'
+import {
+  HTTPStatusCode,
+  type Controller,
+  type HttpRequest,
+  type HttpResponse
 } from '@/presentation/protocols'
 
 export class LogControllerDecorator implements Controller {
-  constructor(private readonly controller: Controller) {}
+  constructor(
+    private readonly controller: Controller,
+    private readonly logErrorRepository: LogErrorRepository
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const httpResponse = await this.controller.handle(httpRequest)
+    await this.handleLogError(httpResponse)
     return httpResponse
+  }
+
+  private async handleLogError(res: HttpResponse): Promise<void> {
+    if (res.statusCode !== HTTPStatusCode.SERVER_ERROR) {
+      return
+    }
+
+    if (!(res.body instanceof Error) || !res.body.stack) {
+      return
+    }
+
+    await this.logErrorRepository.logError(res.body.stack)
   }
 }
