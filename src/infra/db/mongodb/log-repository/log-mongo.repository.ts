56a97b8184c229dac3e53f7ba57d96a414e774a8
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2026-02-13 20:22:31 -0300

    feat: ensure LogMongoRepository create an error on success

---
+++ src/infra/db/mongodb/log-repository/log-mongo.repository.ts
@@ -0,0 +1,9 @@
+import type { LogErrorRepository } from '@/data/protocols'
+import { MongoHelper } from '@/infra/db/mongodb/helpers'
+
+export class LogMongoRepository implements LogErrorRepository {
+  async logError(stack: string): Promise<void> {
+    const collection = MongoHelper.getCollection('errors')
+    await collection.insertOne({ stack, date: new Date() })
+  }
+}
