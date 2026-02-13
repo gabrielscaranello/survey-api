de5980edb6f9cd44bc50673209faca5a18d9bbe4
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2026-02-12 23:56:07 -0300

    feat: ensure SignUp factory uses LogControllerDecorator

--- src/main/factories/signup.factory.ts
+++ src/main/factories/signup.factory.ts
@@ -1,14 +1,20 @@
 import { DbAddAccount } from '@/data/usecases'
 import { BcryptAdapter } from '@/infra/criptography'
 import { AccountMongoRepository } from '@/infra/db/mongodb/account-repository'
+import { LogControllerDecorator } from '@/main/decorators'
 import { SignupController } from '@/presentation/controllers'
+import type { Controller } from '@/presentation/protocols'
 import { EmailValidatorAdapter } from '@/utils'
 
-export const makeSignUp = (): SignupController => {
+export const makeSignUp = (): Controller => {
   const salt = 12
   const emailValidator = new EmailValidatorAdapter()
   const hasher = new BcryptAdapter(salt)
   const addAccountRepository = new AccountMongoRepository()
   const accountRepository = new DbAddAccount(hasher, addAccountRepository)
-  return new SignupController(emailValidator, accountRepository)
+  const signupController = new SignupController(
+    emailValidator,
+    accountRepository
+  )
+  return new LogControllerDecorator(signupController)
 }
