ff3b9bd9e68f40be2666e91713919add7deca4eb
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2026-02-13 20:41:35 -0300

    feat: ensure LogDecorator has injected LogMongoRepository

--- src/main/factories/signup.factory.ts
+++ src/main/factories/signup.factory.ts
@@ -1,6 +1,7 @@
 import { DbAddAccount } from '@/data/usecases'
 import { BcryptAdapter } from '@/infra/criptography'
 import { AccountMongoRepository } from '@/infra/db/mongodb/account-repository'
+import { LogMongoRepository } from '@/infra/db/mongodb/log-repository'
 import { LogControllerDecorator } from '@/main/decorators'
 import { SignupController } from '@/presentation/controllers'
 import type { Controller } from '@/presentation/protocols'
@@ -8,13 +9,16 @@ import { EmailValidatorAdapter } from '@/utils'
 
 export const makeSignUp = (): Controller => {
   const salt = 12
-  const emailValidator = new EmailValidatorAdapter()
   const hasher = new BcryptAdapter(salt)
+
+  const emailValidator = new EmailValidatorAdapter()
   const addAccountRepository = new AccountMongoRepository()
   const accountRepository = new DbAddAccount(hasher, addAccountRepository)
+
   const signupController = new SignupController(
     emailValidator,
     accountRepository
   )
-  return new LogControllerDecorator(signupController)
+  const logErrorRepository = new LogMongoRepository()
+  return new LogControllerDecorator(signupController, logErrorRepository)
 }
