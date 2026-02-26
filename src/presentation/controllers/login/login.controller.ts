466709a29bc3f1bdadd59857198dc65eb16cea03
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2026-02-26 17:10:43 -0300

    feat: ensure LoginController returns 200 if Authentication succeeds

--- src/presentation/controllers/login/login.controller.ts
+++ src/presentation/controllers/login/login.controller.ts
@@ -43,7 +43,7 @@ export class LoginController implements Controller {
         return unauthorized()
       }
 
-      return ok({})
+      return ok({ accessToken })
     } catch (error) {
       return serverError(error)
     }
