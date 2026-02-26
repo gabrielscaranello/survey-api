e9b993307f38457c24fd4926825eedfe49a2e2d6
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2026-02-26 20:15:58 -0300

    test: ensure RequiredFieldValidation works fine

---
+++ src/presentation/helpers/validators/required-field/required-field-validation.spec.ts
@@ -0,0 +1,58 @@
+import { MissingParamError } from '@/presentation/errors'
+
+import { RequiredFieldValidation } from './required-field.validation'
+
+const makeFakeInput = (): Record<string, string> => ({
+  any_field: 'any_value',
+  other_field: 'other_value'
+})
+
+interface SutTypes {
+  sut: RequiredFieldValidation
+  fieldName: string
+}
+
+const makeSut = (): SutTypes => {
+  const fieldName = 'any_field'
+  const sut = new RequiredFieldValidation(fieldName)
+
+  return { sut, fieldName }
+}
+
+describe('RequiredFieldValidation', () => {
+  it('should return a MissingParamError if field is missing', () => {
+    const { sut, fieldName } = makeSut()
+
+    const error = sut.validate({ other_field: 'any_value' })
+
+    expect(error).toEqual(new MissingParamError(fieldName))
+  })
+
+  it('should return a MissingParamError if field is null', () => {
+    const { sut, fieldName } = makeSut()
+    const error = sut.validate({ [fieldName]: null })
+
+    expect(error).toEqual(new MissingParamError(fieldName))
+  })
+
+  it('should return a MissingParamError if field is empty', () => {
+    const { sut, fieldName } = makeSut()
+    const error = sut.validate({ [fieldName]: '' })
+
+    expect(error).toEqual(new MissingParamError(fieldName))
+  })
+
+  it('should return a MissingParamError if field is undefined', () => {
+    const { sut, fieldName } = makeSut()
+    const error = sut.validate({ [fieldName]: undefined })
+
+    expect(error).toEqual(new MissingParamError(fieldName))
+  })
+
+  it('should return null validation success', () => {
+    const { sut } = makeSut()
+    const error = sut.validate(makeFakeInput())
+
+    expect(error).toBeNull()
+  })
+})
