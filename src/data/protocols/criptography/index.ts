d48be46d54a7dfa27f194bea96e85b98f3aa5366
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2026-03-01 02:01:19 -0300

    feat: ensure Authentication calls TokenGenerator with correct accountId

--- src/data/protocols/criptography/index.ts
+++ src/data/protocols/criptography/index.ts
@@ -1,2 +1,3 @@
 export type * from './hasher'
 export type * from './hash-comparer'
+export type * from './token-generator'
--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,13 +1,15 @@
 import type {
   HashComparer,
-  LoadAccountByEmailRepository
+  LoadAccountByEmailRepository,
+  TokenGenerator
 } from '@/data/protocols'
 import type { Authentication, AuthenticationParams } from '@/domain/usecases'
 
 export class DbAuthentication implements Authentication {
   constructor(
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
-    private readonly hashComparer: HashComparer
+    private readonly hashComparer: HashComparer,
+    private readonly tokenGenerator: TokenGenerator
   ) {}
 
   async auth(params: AuthenticationParams): Promise<string | null> {
@@ -17,6 +19,7 @@ export class DbAuthentication implements Authentication {
     if (!account) return null
 
     await this.hashComparer.compare(password, account.password)
+    await this.tokenGenerator.generateToken(account.id)
     return null
   }
 }
