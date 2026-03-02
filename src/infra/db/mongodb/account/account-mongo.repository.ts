f46dba576270d16885dfe789e457c575cb0d90b9
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2026-03-02 00:55:56 -0300

    feat: ensure AccountMongoRepository returns null if no account is found

--- src/infra/db/mongodb/account/account-mongo.repository.ts
+++ src/infra/db/mongodb/account/account-mongo.repository.ts
@@ -1,12 +1,21 @@
-import type { AddAccountRepository } from '@/data/protocols'
+import type {
+  AddAccountRepository,
+  LoadAccountByEmailRepository
+} from '@/data/protocols'
 import type { AccountModel } from '@/domain/models'
 import type { AddAccountModel } from '@/domain/usecases'
 import { MongoHelper } from '@/infra/db/mongodb/helpers'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository
+  implements AddAccountRepository, LoadAccountByEmailRepository
+{
   async add(data: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollection('accounts')
     const { insertedId } = await accountCollection.insertOne({ ...data })
     return { ...data, id: insertedId.toString() }
+  }
+
+  async loadByEmail(_: string): Promise<AccountModel | null> {
+    return await Promise.resolve(null)
   }
 }
