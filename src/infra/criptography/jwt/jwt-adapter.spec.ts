f0910eb14a79e9494aa90d447a8afc007c4e72af
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2026-03-03 02:05:02 -0300

    refactor: move JWTAdapter sut creation to a helper factory method

--- src/infra/criptography/jwt/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt/jwt-adapter.spec.ts
@@ -5,16 +5,18 @@ vi.mock('jsonwebtoken', () => ({
   sign: (...args: []): any => mockedSign(...args)
 }))
 
+const makeSut = (): JWTAdapter => new JWTAdapter('secret')
+
 describe('JWT Adapter', () => {
   it('should call sign with correct values', async () => {
-    const sut = new JWTAdapter('secret')
+    const sut = makeSut()
     await sut.encrypt('any_id')
 
     expect(mockedSign).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
   })
 
   it('should return a token on sign success', async () => {
-    const sut = new JWTAdapter('secret')
+    const sut = makeSut()
 
     const accessToken = await sut.encrypt('any_id')
 
@@ -22,10 +24,11 @@ describe('JWT Adapter', () => {
   })
 
   it('should throw if sign throws', async () => {
-    const sut = new JWTAdapter('secret')
+    const sut = makeSut()
     mockedSign.mockImplementationOnce(() => {
       throw new Error()
     })
+
     const promise = sut.encrypt('any_id')
 
     await expect(promise).rejects.toThrow()
