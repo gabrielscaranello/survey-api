49624d55d431a4a353de299cbc8300eaf5e6425a
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2026-03-03 01:57:51 -0300

    test: ensure JWTAdapter returns generated token

--- src/infra/criptography/jwt/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt/jwt-adapter.spec.ts
@@ -1,6 +1,6 @@
 import { JWTAdapter } from './jwt.adapter'
 
-const mockedSign = vi.fn()
+const mockedSign = vi.fn().mockReturnValue('any_token')
 vi.mock('jsonwebtoken', () => ({
   sign: (...args: []): any => mockedSign(...args)
 }))
@@ -11,5 +11,13 @@ describe('JWT Adapter', () => {
     await sut.encrypt('any_id')
 
     expect(mockedSign).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+  })
+
+  it('should return a token on sign success', async () => {
+    const sut = new JWTAdapter('secret')
+
+    const accessToken = await sut.encrypt('any_id')
+
+    expect(accessToken).toBe('any_token')
   })
 })
