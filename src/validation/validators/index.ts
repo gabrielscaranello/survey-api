8b85e59a499b16cf7bed2fc6477694cb80a64e71
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2026-03-14 16:14:19 -0300

    refactor: review validation layer architecture

--- src/validation/validators/index.ts
+++ src/validation/validators/index.ts
@@ -1,5 +1,5 @@
 // Module exports
-export * from './compare-field.validation'
-export * from './email.validation'
-export * from './required-field.validation'
+export * from './compare-field-validation'
+export * from './email-validation'
+export * from './required-field-validation'
 export * from './validation-composite'
