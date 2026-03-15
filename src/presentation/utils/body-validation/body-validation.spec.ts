be891464a1e444964181ea6b220962931d1e2a0a
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2026-03-15 18:16:55 -0300

    feat: ensure bodyValidation returns an body if all validations pass

--- src/presentation/utils/body-validation/body-validation.spec.ts
+++ src/presentation/utils/body-validation/body-validation.spec.ts
@@ -1,4 +1,4 @@
-import type { Validation } from '@/presentation/protocols'
+import type { HttpRequest, Validation } from '@/presentation/protocols'
 
 import { InvalidRequestError } from '@/presentation/errors'
 
@@ -12,11 +12,22 @@ class ValidationStub implements Validation {
 
 const validationStub = new ValidationStub()
 
+const mockRequest = (): HttpRequest<Record<string, string>> => ({
+  body: { any_field: 'any_value' }
+})
+
 describe('Body Validation', () => {
   it('should return an error if no body is provided', () => {
     const { error, body } = sut({ body: undefined }, validationStub)
 
     expect(error).toEqual(new InvalidRequestError('no request body provided'))
     expect(body).toBe(null)
+  })
+
+  it('should return body if all validations pass', () => {
+    const { error, body } = sut(mockRequest(), validationStub)
+
+    expect(error).toBe(null)
+    expect(body).toEqual(mockRequest().body)
   })
 })
--- src/presentation/utils/body-validation/body-validation.ts
+++ src/presentation/utils/body-validation/body-validation.ts
@@ -4,9 +4,15 @@ import type { BodyValidationResult } from './body-validation.types'
 import { InvalidRequestError } from '@/presentation/errors'
 
 export const bodyValidation = <T>(
-  _request: HttpRequest,
+  request: HttpRequest<T>,
   _validation: Validation
-): BodyValidationResult<T> => ({
-  body: null,
-  error: new InvalidRequestError('no request body provided')
-})
+): BodyValidationResult<T> => {
+  if (!request.body) {
+    return {
+      error: new InvalidRequestError('no request body provided'),
+      body: null
+    }
+  }
+
+  return { error: null, body: request.body }
+}
